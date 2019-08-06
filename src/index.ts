2c93de1003be37b8eb97f436f06e48617e374ebd
samlazrak <samlazrak@uab.edu>
2019-08-06 18:09:54 -0500

    init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import 'reflect-metadata';
+
+import { GraphQLServer } from 'graphql-yoga';
+import { importSchema } from 'graphql-import';
+import { makeExecutableSchema } from 'graphql-tools';
+import { resolvers } from './resolvers';
+import { createConnection } from 'typeorm';
+
+const typeDefs = importSchema('src/schema.graphql');
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+const server = new GraphQLServer({ schema });
+
+createConnection().then((_connection) => {
+  server.start(() => console.log(`🚀 Server ready at http://localhost:4000`));
+});
